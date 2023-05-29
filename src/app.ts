5aef5796b534fd487ec0aac1b80fff0138ce1887
Filip Lulkiewicz <f.lulkiewicz@gmail.com>
2023-05-29 14:00:18 +0200

    fix: db connection string moved to .env, added dbMovie API key and token

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,41 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 
-import HttpError from './models/http-error';
-import { routes } from "./routes"
+import HttpError from './models/http-error'
+import { routes } from './routes'
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
 
-  next();
-});
+	next()
+})
 
 app.use('/api', routes)
 
 app.use((req, res, next) => {
-  const error = new HttpError('This route is not available', 404);
-  return next(error);
-});
+	const error = new HttpError('This route is not available', 404)
+	return next(error)
+})
 
 app.use((err: any, req: any, res: any, next: any) => {
-  if (res.headersSent) {
-    return next(err);
-  }
-  res.status(err.code || 500);
-  res.json({ message: err.message || 'Unknown error occurred.' });
-});
+	if (res.headersSent) {
+		return next(err)
+	}
+	res.status(err.code || 500)
+	res.json({ message: err.message || 'Unknown error occurred.' })
+})
 
 mongoose
-  .connect('mongodb+srv://wsb123:wsb123@atlascluster.rv2ka7q.mongodb.net/?retryWrites=true&w=majority')
-  .then(() => {
-    app.listen(5000);
-  })
-  .catch((err) => {
-    console.log('Connection error: ', err);
-  });
+	.connect(process.env.MONGO_URI!)
+	.then(() => {
+		app.listen(5000)
+	})
+	.catch(err => {
+		console.log('Connection error: ', err)
+	})
